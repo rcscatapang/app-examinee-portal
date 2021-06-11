8c86d8eedcb7e1e7776cd7df9e1199c62e8c1cd9
Ryan Catapang <rcscatapang96@gmail.com>
2021-06-12 00:14:48 +0800

    Updated student exam index page with ongoing and upcoming exam data

--- app/Http/Controllers/Student/ExamsController.php
+++ app/Http/Controllers/Student/ExamsController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers\Student;
 
+use App\Enums\ExamStatus;
 use App\Http\Controllers\Controller;
 use App\Models\Exam;
 use App\Models\Question;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ExamsController extends Controller
 {
     public function index()
     {
-        return view('student.exams.index');
+        $student = auth()->user()->student;
+        $courses = $student->courses->pluck('id');
+
+        $current_date = Carbon::now();
+        $upcoming_exams = Exam::whereIn('course_id', $courses)->where('status', ExamStatus::Published)
+            ->whereDate('start_date', '>', $current_date)->orderBy('start_date', 'desc')->get();
+        $ongoing_exams = Exam::whereIn('course_id', $courses)->where('status', ExamStatus::Published)
+            ->whereDate('start_date', '<=', $current_date)->orderBy('start_date', 'desc')->get();
+
+        return view('student.exams.index', compact(['upcoming_exams', 'ongoing_exams']));
     }
 
     public function show(Exam $exam)
--- resources/views/student/exams/index.blade.php
+++ resources/views/student/exams/index.blade.php
@@ -20,10 +20,72 @@
             <div class="col">
                 <div class="card-wrapper">
                     <div class="card">
-                        <div class="card-header">
-                            <h3 class="mb-0 font-weight-900">Upcoming</h3>
-                        </div>
                         <div class="card-body">
+                            <h3 class="mb-2 font-weight-900">Upcoming</h3>
+                            <ul class="list-group list-group-flush list">
+                                @if(count($upcoming_exams) > 0)
+                                    @foreach($upcoming_exams as $exam)
+                                        <li class="list-group-item px-0 border-top">
+                                            <div class="row align-items-center">
+                                                <div class="col-auto">
+                                                    <i class="fas fa-file-alt"></i>
+                                                </div>
+                                                <div class="col">
+                                                    <a href="{{ route('exams.show', $exam->id) }}">
+                                                        <small>{{ $exam->code }} - {{ $exam->type }}</small>
+                                                    </a>
+                                                    <h5 class="mb-0">{{ $exam->course->name }}</h5>
+                                                </div>
+                                                <div class="col">
+                                                    <small>Start date:</small>
+                                                    <h5 class="mb-0">{{ $exam->start_date }}</h5>
+                                                </div>
+                                                <div class="col">
+                                                    <small>End date:</small>
+                                                    <h5 class="mb-0">{{ $exam->end_date }}</h5>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                @else
+                                    <div class="border-top">
+                                        <h4 class="font-italic font-weight-300 pt-2"> No records yet — </h4>
+                                    </div>
+                                @endif
+                            </ul>
+
+                            <h3 class="mb-2 mt-4 font-weight-900">Ongoing</h3>
+                            <ul class="list-group list-group-flush list">
+                                @if(count($ongoing_exams) > 0)
+                                    @foreach($ongoing_exams as $exam)
+                                        <li class="list-group-item px-0 border-top">
+                                            <div class="row align-items-center">
+                                                <div class="col-auto">
+                                                    <i class="fas fa-pen"></i>
+                                                </div>
+                                                <div class="col">
+                                                    <a href="{{ route('exams.show', $exam->id) }}">
+                                                        <small>{{ $exam->code }} - {{ $exam->type }}</small>
+                                                    </a>
+                                                    <h5 class="mb-0">{{ $exam->course->name }}</h5>
+                                                </div>
+                                                <div class="col">
+                                                    <small>Start date:</small>
+                                                    <h5 class="mb-0">{{ $exam->start_date }}</h5>
+                                                </div>
+                                                <div class="col">
+                                                    <small>End date:</small>
+                                                    <h5 class="mb-0">{{ $exam->end_date }}</h5>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                @else
+                                    <div class="border-top">
+                                        <h4 class="font-italic font-weight-300 pt-2"> No records yet — </h4>
+                                    </div>
+                                @endif
+                            </ul>
                         </div>
                     </div>
                     <div class="card">
